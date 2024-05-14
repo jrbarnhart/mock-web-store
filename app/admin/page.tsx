6e572fe94ed5572fcee8d995b475a5fe23f233fd
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-05-14 17:28:02 -0400

    Add fn for getting user data and implement into card

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -20,14 +20,42 @@ async function getSalesData() {
   };
 }
 
+async function getUserData() {
+  const [userCount, orderData] = await Promise.all([
+    prisma.user.count(),
+    prisma.order.aggregate({
+      _sum: { totalPaidInCents: true },
+    }),
+  ]);
+
+  return {
+    userCount,
+    averagePaidPerUser:
+      userCount === 0
+        ? 0
+        : (orderData._sum.totalPaidInCents || 0) / userCount / 100,
+  };
+}
+
 export default async function AdminDashboard() {
-  const salesData = await getSalesData();
+  const [salesData, userData] = await Promise.all([
+    getSalesData(),
+    getUserData(),
+  ]);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <DashboardCard
         title="Sales"
-        subtitle={formatNumber(salesData.numberOfSales)}
+        subtitle={`${formatNumber(salesData.numberOfSales)} Orders`}
         body={formatCurrency(salesData.amount)}
+      />
+      <DashboardCard
+        title="Customers"
+        subtitle={`${formatCurrency(
+          userData.averagePaidPerUser
+        )} Average Value`}
+        body={`${formatNumber(userData.userCount)}`}
       />
     </div>
   );
