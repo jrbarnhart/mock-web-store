a0a300e52577ee9555e0992ef9eee20bc45f09c4
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-05-14 15:37:22 -0400

    Remove default nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
 import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
@@ -24,13 +23,6 @@ export default function RootLayout({
           inter.variable
         )}
       >
-        <nav>
-          <ul className="grid grid-flow-col gap-y-3 justify-items-center p-6 bg-zinc-300 font-bold text-lg">
-            <Link href="/">Home</Link>
-            <Link href="/products">Products</Link>
-            <Link href="/dashboard">Admin</Link>
-          </ul>
-        </nav>
         {children}
       </body>
     </html>
