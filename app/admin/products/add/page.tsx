5e99db99abf42b706f81531e71f85e6d7dd30695
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-05-15 15:56:49 -0400

    Add page for add product

---
+++ app/admin/products/add/page.tsx
@@ -0,0 +1,9 @@
+import { PageHeader } from "@/components/admin/PageHeader";
+
+export default function AddProductPage() {
+  return (
+    <>
+      <PageHeader>Add Product</PageHeader>
+    </>
+  );
+}
