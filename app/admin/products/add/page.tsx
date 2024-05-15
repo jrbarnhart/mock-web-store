b641d52a050ea7b8458b04624ab24e0f179bb81e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-05-15 16:06:52 -0400

    Add the add product form component

--- app/admin/products/add/page.tsx
+++ app/admin/products/add/page.tsx
@@ -1,9 +1,11 @@
+import { AddProductForm } from "@/components/admin/AddProductForm";
 import { PageHeader } from "@/components/admin/PageHeader";
 
 export default function AddProductPage() {
   return (
     <>
       <PageHeader>Add Product</PageHeader>
+      <AddProductForm />
     </>
   );
 }
