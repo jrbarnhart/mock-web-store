a05c2c3cb5b5dfd4da7562b4ac1ae16bf263de13
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-05-19 18:48:53 -0400

    Add file for product server actions

---
+++ app/admin/_actions/productActions.ts
@@ -0,0 +1,11 @@
+import prisma from "@/components/db/db";
+
+export async function toggleProductAvailable(
+  id: string,
+  availableForPurchase: boolean
+) {
+  await prisma.product.update({
+    where: { id },
+    data: { availableForPurchase },
+  });
+}
