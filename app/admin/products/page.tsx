4db986b051e2c61fe4c5de6660cbd43f7ccfe65e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-05-19 18:28:12 -0400

    Add table cells for products

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,12 +1,16 @@
 import { PageHeader } from "@/components/admin/PageHeader";
+import prisma from "@/components/db/db";
 import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
+  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
+import { CheckCircle, MoreVertical, XCircle } from "lucide-react";
 import Link from "next/link";
 
 export default function AdminProductsPage() {
@@ -23,7 +27,21 @@ export default function AdminProductsPage() {
   );
 }
 
-function ProductsTable() {
+async function ProductsTable() {
+  const products = await prisma.product.findMany({
+    select: {
+      id: true,
+      name: true,
+      description: true,
+      priceInCents: true,
+      availableForPurchase: true,
+      _count: { select: { orderItems: true } },
+    },
+    orderBy: { name: "asc" },
+  });
+
+  if (products.length === 0) return <p>No products found.</p>;
+
   return (
     <Table>
       <TableHeader>
@@ -39,7 +57,32 @@ function ProductsTable() {
           </TableHead>
         </TableRow>
       </TableHeader>
-      <TableBody></TableBody>
+      <TableBody>
+        {products.map((product) => (
+          <TableRow key={product.id}>
+            <TableCell>
+              {product.availableForPurchase ? (
+                <>
+                  <CheckCircle />
+                  <span className="sr-only">Available</span>
+                </>
+              ) : (
+                <>
+                  <XCircle />
+                  <span className="sr-only">Unavailable</span>
+                </>
+              )}
+            </TableCell>
+            <TableCell>{product.name}</TableCell>
+            <TableCell>{formatCurrency(product.priceInCents / 100)}</TableCell>
+            <TableCell>{formatNumber(product._count.orderItems)}</TableCell>
+            <TableCell>
+              <MoreVertical />
+              <span className="sr-only">Actions</span>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
     </Table>
   );
 }
