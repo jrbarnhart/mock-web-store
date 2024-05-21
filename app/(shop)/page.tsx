de7949947eb35e3e73f92d1befc171bf263d6416
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-05-21 15:29:08 -0400

    Add button links to products page

--- app/(shop)/page.tsx
+++ app/(shop)/page.tsx
@@ -1,5 +1,8 @@
+import { Button } from "@/components/ui/button";
 import { getPopularProducts, getRecentProducts } from "@/lib/queryProducts";
 import { ProductFetcher } from "@/lib/types";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
@@ -24,6 +27,12 @@ function ProductGridSection({
     <div className="space-y-4">
       <div className="flex gap-4">
         <h2 className="text-2xl font-bold">{title}</h2>
+        <Button variant="outline" asChild>
+          <Link className="space-x-2" href="/products">
+            <span>View All</span>
+            <ArrowRight className="size-4" />
+          </Link>
+        </Button>
       </div>
     </div>
   );
