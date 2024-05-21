fc5828a11d8ae1efeac6cfbfaede7acb09916a36
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-05-21 17:17:50 -0400

    Add basic page content with skeletons and cards

--- app/(shop)/products/page.tsx
+++ app/(shop)/products/page.tsx
@@ -1,3 +1,43 @@
-export default function Page() {
-  return <p>This is the products page!</p>;
+import ProductCard, {
+  ProductCardSkeleton,
+} from "@/components/shop/products/ProductCard";
+import { getAvailableProducts } from "@/lib/queryProducts";
+import { Suspense } from "react";
+
+export default function ProductsPage() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <Suspense
+        fallback={
+          <>
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+          </>
+        }
+      >
+        <Suspense
+          fallback={
+            <>
+              <ProductCardSkeleton />
+              <ProductCardSkeleton />
+              <ProductCardSkeleton />
+              <ProductCardSkeleton />
+              <ProductCardSkeleton />
+              <ProductCardSkeleton />
+            </>
+          }
+        >
+          <ProductsSuspense />
+        </Suspense>
+      </Suspense>
+    </div>
+  );
+}
+
+async function ProductsSuspense() {
+  const products = await getAvailableProducts();
+  return products.map((product) => (
+    <ProductCard key={product.id} {...product} />
+  ));
 }
