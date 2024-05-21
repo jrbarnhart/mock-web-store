9c4a92de63a1863dc5359aa161283feba70074ce
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-05-21 15:23:41 -0400

    Add product sections to page

--- app/(shop)/page.tsx
+++ app/(shop)/page.tsx
@@ -1,9 +1,30 @@
-import Image from "next/image";
+import { getPopularProducts, getRecentProducts } from "@/lib/queryProducts";
+import { ProductFetcher } from "@/lib/types";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>This is some main content!</p>
+    <main className="space-y-12">
+      <ProductGridSection
+        title="Trending"
+        productFetcher={getPopularProducts}
+      />
+      <ProductGridSection title="New" productFetcher={getRecentProducts} />
     </main>
+  );
+}
+
+function ProductGridSection({
+  productFetcher,
+  title,
+}: {
+  productFetcher: ProductFetcher;
+  title: string;
+}) {
+  return (
+    <div className="space-y-4">
+      <div className="flex gap-4">
+        <h2 className="text-2xl font-bold">{title}</h2>
+      </div>
+    </div>
   );
 }
